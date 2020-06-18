a2af5d9527795250d23519fb142ffc8f836b3a00
Edison Yang <edison.cy.yang@gmail.com>
2020-06-18 11:32:04 -0700

    include the send sms block for async execution on a dispatch main queue

--- src/ios/Sms.m
+++ src/ios/Sms.m
@@ -26,28 +26,30 @@
             return;
         }
         
-        MFMessageComposeViewController *composeViewController = [[MFMessageComposeViewController alloc] init];
-        composeViewController.messageComposeDelegate = self;
-        
-        NSString* body = [command.arguments objectAtIndex:1];
-        if (body != nil) {
-            BOOL replaceLineBreaks = [[command.arguments objectAtIndex:3] boolValue];
-            if (replaceLineBreaks) {
-                body = [body stringByReplacingOccurrencesOfString: @"\\n" withString: @"\n"];
-            }
-            [composeViewController setBody:body];
-        }
-        
-        NSMutableArray* recipients = [command.arguments objectAtIndex:0];
-        if (recipients != nil) {
-            if ([recipients.firstObject isEqual: @""]) {
-                [recipients replaceObjectAtIndex:0 withObject:@"?"];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            MFMessageComposeViewController *composeViewController = [[MFMessageComposeViewController alloc] init];
+            composeViewController.messageComposeDelegate = self;
+            
+            NSString* body = [command.arguments objectAtIndex:1];
+            if (body != nil) {
+                BOOL replaceLineBreaks = [[command.arguments objectAtIndex:3] boolValue];
+                if (replaceLineBreaks) {
+                    body = [body stringByReplacingOccurrencesOfString: @"\\n" withString: @"\n"];
+                }
+                [composeViewController setBody:body];
             }
             
-            [composeViewController setRecipients:recipients];
-        }
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self.viewController presentViewController:composeViewController animated:YES completion:nil];
+            NSMutableArray* recipients = [command.arguments objectAtIndex:0];
+            if (recipients != nil) {
+                if ([recipients.firstObject isEqual: @""]) {
+                    [recipients replaceObjectAtIndex:0 withObject:@"?"];
+                }
+                
+                [composeViewController setRecipients:recipients];
+            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.viewController presentViewController:composeViewController animated:YES completion:nil];
+            });
         });
     }];
 }
